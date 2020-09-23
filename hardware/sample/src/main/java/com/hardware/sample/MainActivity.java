7319fb96e1542d60b29e5058b42cc90ebf827a9e
ubuntu-dunn <dunnsmailface@gmail.com>
2020-09-23 15:01:20 +0800

    action:update

---
+++ hardware/sample/src/main/java/com/hardware/sample/MainActivity.java
@@ -0,0 +1,111 @@
+package com.hardware.sample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.hardware.sample.data.CustomAdapter;
+import com.hardware.sample.data.CustomBean;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView list;
+    private CustomAdapter mAdapter;
+    private ArrayList<CustomBean> mListData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = findViewById(R.id.list);
+
+        initValue();
+        initListen();
+    }
+
+    private void initValue(){
+        mListData = new ArrayList<>();
+
+        //add circleTick
+        CustomBean beanCircleTick = new CustomBean("circleTick",
+                "１,父布局自适应大小 2,测量指定大小 3,onDraw",
+                "com.example.sample.widget.CircleTickActivity");
+        mListData.add(beanCircleTick);
+        //add circlePan
+        CustomBean beanCirclePan = new CustomBean("circlePan",
+                "１,父布局指定大小 2,资源自适应大小 3,onLayout",
+                "com.example.sample.widget.CirclePanActivity");
+        mListData.add(beanCirclePan);
+        //add calendar
+        CustomBean beanCalendar = new CustomBean("calendar",
+                "１,父布局自适应大小 2,测量指定大小 3,onDraw",
+                "com.example.sample.widget.CalendarActivity");
+        mListData.add(beanCalendar);
+
+        CustomBean beanDraw = new CustomBean("draw",
+                "区域绘制",
+                "com.example.sample.widget.MoveDarwActivity");
+        mListData.add(beanDraw);
+
+        CustomBean beanlight = new CustomBean("Light",
+                "智能设想灯",
+                "com.example.sample.widget.LightActivity");
+        mListData.add(beanlight);
+
+
+        CustomBean beanTimeBar = new CustomBean("TimeBar",
+                "时间轴",
+                "com.example.sample.widget.TimeBarActivity");
+        mListData.add(beanTimeBar);
+
+        //add animation
+        CustomBean beanScanAnimation = new CustomBean("scanAnimation",
+                "scanAnimation",
+                "com.example.sample.animation.ScanAnimationActivity");
+        mListData.add(beanScanAnimation);
+        CustomBean beanLoadingAnimation = new CustomBean("loadingAnimation",
+                "loadingAnimation",
+                "com.example.sample.animation.LoadingAnimationActivity");
+        mListData.add(beanLoadingAnimation);
+        CustomBean beanFallingBallAnimation = new CustomBean("fallingBallAnimation",
+                "fallingBallAnimation",
+                "com.example.sample.animation.FallingBallAnimationActivity");
+        mListData.add(beanFallingBallAnimation);
+        CustomBean beanMenuAnimation = new CustomBean("menuAnimation",
+                "menuAnimation",
+                "com.example.sample.animation.MenuAnimationActivity");
+        mListData.add(beanMenuAnimation);
+        CustomBean beanTanAnimation = new CustomBean("tanAnimation",
+                "tanAnimation",
+                "com.example.sample.animation.TanAnimationActivity");
+        mListData.add(beanTanAnimation);
+
+        //add lock
+        CustomBean beanLock = new CustomBean("lock",
+                "密码锁",
+                "com.example.sample.widget.CircleLockActivity");
+        mListData.add(beanLock);
+
+        mAdapter = new CustomAdapter(MainActivity.this,mListData);
+        list.setAdapter(mAdapter);
+    }
+
+    private void initListen(){
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CustomBean bean = mListData!=null?mListData.get(position):null;
+                if(bean!=null){
+                    Intent in = new Intent();
+                    in.setClassName(MainActivity.this, bean.getActivityUri());
+                    MainActivity.this.startActivity(in);
+                }
+            }
+        });
+    }
+}
---
+++ hardware/sample/src/main/java/com/hardware/sample/widget/CircleLockActivity.java
@@ -0,0 +1,56 @@
+package com.hardware.sample.widget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.hardware.sample.R;
+import com.tc.lock.CircleLock;
+
+
+public class CircleLockActivity extends AppCompatActivity {
+    private CircleLock mCircleLock;
+    private TextView valueTV1,valueTV2,valueTV3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circlelock);
+
+        valueTV1 = (TextView)findViewById(R.id.valuetv1);
+        valueTV2 = (TextView)findViewById(R.id.valuetv2);
+        valueTV3 = (TextView)findViewById(R.id.valuetv3);
+        mCircleLock = (CircleLock)findViewById(R.id.crpv_tick);
+        mCircleLock.setOnTimeChangeListener(new CircleLock.OnTimeChangeListener() {
+            @Override
+            public void onChange(int value1, int value2, int value3) {
+                Log.v("lock", "main onChange value1="+value1+", value2="+value2+", value3="+value3);
+                valueTV3.setText(value1<10 ? ("0"+value1) : (value1+""));
+                valueTV2.setText(value2<10 ? ("0"+value2) : (value2+""));
+                valueTV1.setText(value3<10 ? ("0"+value3) : (value3+""));
+            }
+
+            @Override
+            public void updateSingle(int index, int value) {
+                if(index==1){
+                    valueTV3.setText(value<10 ? ("0"+value) : (value+""));
+                }else if(index==2){
+                    valueTV2.setText(value<10 ? ("0"+value) : (value+""));
+                }else if(index==3){
+                    valueTV1.setText(value<10 ? ("0"+value) : (value+""));
+                }
+            }
+//
+//            @Override
+//            public void onChange(int value) {
+//                //Log.v("lock", "main onChange value="+value);
+//            }
+        });
+    }
+
+    public void onBack(View view){
+        finish();
+    }
+}
